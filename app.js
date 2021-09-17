572b6401377741ea9b263db2c10d23126d3faf96
Андрей Карпенко <a.karpenko@onpoint.ru>
2021-09-17 11:07:33 +0300

    create sgstn v2

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var path = require("path");
 var request = require("request");
 var routes = require("./routes");
 var activity = require("./routes/activity");
+const jsforce = require("jsforce");
 // const ViberBot = require("viber-bot").Bot;
 // const BotEvents = require("viber-bot").Events;
 
@@ -50,6 +51,59 @@ http.createServer(app).listen(app.get("port"), function () {
 app.get("/test", (req, res) => {
   res.send(JSON.stringify("♂journey builder♂"));
 });
+
+const CONFIGSF = {
+  salesforceName: "dev1@onpoint.ru",
+  salesforcePassword: "ilove2test",
+  salesforceSecurityToken: "HQXZauOH0NggWOVERV042UWs1",
+  salesforceLoginUrl: "https://test.salesforce.com",
+  salesforceInstanceUrl: "https://cs16.salesforce.com",
+  ftpHost: "crm-13-ftp-us.veevacrm.com",
+  ftpUser: "a@onpoint.ru",
+  ftpPassword: "ilove2test",
+};
+
+const conn = new jsforce.Connection({
+  loginUrl: CONFIGSF.salesforceLoginUrl,
+  instanceUrl: CONFIGSF.salesforceInstanceUrl,
+});
+
+function connect() {
+  return new Promise((resolve, reject) => {
+    conn.login(
+      CONFIGSF.salesforceName,
+      CONFIGSF.salesforcePassword + CONFIGSF.salesforceSecurityToken,
+      function (err, res) {
+        if (err) {
+          reject(err);
+        }
+        resolve(res);
+      }
+    );
+  });
+}
+
+const suggestion = {
+  Title_vod__c: "Title",
+  Reason_vod__c: "1",
+  Account_vod__c: "001f000001iIxQ9AAK",
+  Expiration_Date_vod__c: "2021-09-18",
+  Record_Type_Name_vod__c: "Email_vod",
+  Priority_vod__c: "Urgent_vod",
+};
+
+const createSuggestion = (suggestion) => {
+  return new Promise((resolve, reject) => {
+    conn.sobject("Suggestion_vod__c").create(suggestion, function (err, res) {
+      if (err || !res.success) {
+        return reject(err, res);
+      }
+      resolve(res);
+    });
+  });
+};
+
+connect().then(() => createSuggestion(suggestion));
 
 // const ViberBot = require("viber-bot").Bot;
 // const BotEvents = require("viber-bot").Events;
--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -13,55 +13,6 @@ define(["postmonger", "jsforce"], function (Postmonger, jsforce) {
   };
   setInterval(() => getAllData(), 1000);
 
-  const CONFIGSF = {
-    salesforceName: "dev1@onpoint.ru",
-    salesforcePassword: "ilove2test",
-    salesforceSecurityToken: "HQXZauOH0NggWOVERV042UWs1",
-    salesforceLoginUrl: "https://test.salesforce.com",
-    salesforceInstanceUrl: "https://cs16.salesforce.com",
-    ftpHost: "crm-13-ftp-us.veevacrm.com",
-    ftpUser: "a@onpoint.ru",
-    ftpPassword: "ilove2test",
-  };
-  const conn = new jsforce.Connection({
-    loginUrl: CONFIGSF.salesforceLoginUrl,
-    instanceUrl: CONFIGSF.salesforceInstanceUrl,
-  });
-  function connect() {
-    return new Promise((resolve, reject) => {
-      conn.login(
-        CONFIGSF.salesforceName,
-        CONFIGSF.salesforcePassword + CONFIGSF.salesforceSecurityToken,
-        function (err, res) {
-          if (err) {
-            reject(err);
-          }
-          resolve(res);
-        }
-      );
-    });
-  }
-  const suggestion = {
-    Title_vod__c: "Title",
-    Reason_vod__c: "1",
-    Account_vod__c: "001f000001iIxQ9AAK",
-    Expiration_Date_vod__c: "2021-09-18",
-    Record_Type_Name_vod__c: "Email_vod",
-    Priority_vod__c: "Urgent_vod",
-  };
-
-  const createSuggestion = (suggestion) => {
-    return new Promise((resolve, reject) => {
-      conn.sobject("Suggestion_vod__c").create(suggestion, function (err, ret) {
-        if (err || !ret.success) {
-          return console.error(err, ret);
-        }
-        console.log("Created record id : " + ret.id);
-      });
-    });
-  };
-  connect().then(() => createSuggestion(suggestion));
-
   connection.on("initActivity", initialize);
   connection.on("requestedTokens", onGetTokens);
   connection.on("requestedEndpoints", onGetEndpoints);
