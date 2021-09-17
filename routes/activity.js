3ae328ae6a62edcd63637b39b868b42418af9262
Андрей Карпенко <a.karpenko@onpoint.ru>
2021-09-17 19:00:13 +0300

    ласт трай на сегодня

--- routes/activity.js
+++ routes/activity.js
@@ -1,118 +1,176 @@
-'use strict';
-var util = require('util');
+"use strict";
+var util = require("util");
 
 // Deps
-const Path = require('path');
-const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
-var util = require('util');
-var http = require('https');
+const Path = require("path");
+const JWT = require(Path.join(__dirname, "..", "lib", "jwtDecoder.js"));
+var util = require("util");
+var http = require("https");
+const jsforce = require("jsforce");
+const CONFIGSF = {
+  salesforceName: "dev1@onpoint.ru",
+  salesforcePassword: "ilove2test",
+  salesforceSecurityToken: "4HWqiLg8IV374nkdpYhIBeVBc",
+  salesforceLoginUrl: "https://test.salesforce.com",
+  salesforceInstanceUrl: "https://cs16.salesforce.com",
+};
+
+const conn = new jsforce.Connection({
+  loginUrl: CONFIGSF.salesforceLoginUrl,
+  instanceUrl: CONFIGSF.salesforceInstanceUrl,
+});
+
+function connect() {
+  return new Promise((resolve, reject) => {
+    conn.login(
+      CONFIGSF.salesforceName,
+      CONFIGSF.salesforcePassword + CONFIGSF.salesforceSecurityToken,
+      function (err, res) {
+        if (err) {
+          reject(err);
+        }
+        resolve(res);
+      }
+    );
+  });
+}
+const suggestion = {
+  Title_vod__c: "Ваше письмо не прочитали",
+  Account_vod__c: "001f000001iIxQ9AAK",
+  Expiration_Date_vod__c: "2021-10-17",
+  Record_Type_Name_vod__c: "Email_vod",
+  Priority_vod__c: "Urgent_vod",
+};
+
+const createSuggestion = (suggestion) => {
+  return new Promise((resolve, reject) => {
+    conn.sobject("Suggestion_vod__c").create(suggestion, function (err, res) {
+      if (err || !res.success) {
+        return reject(err, res);
+      }
+      resolve(res);
+    });
+  });
+};
 
 exports.logExecuteData = [];
 
 function logData(req) {
-    exports.logExecuteData.push({
-        body: req.body,
-        headers: req.headers,
-        trailers: req.trailers,
-        method: req.method,
-        url: req.url,
-        params: req.params,
-        query: req.query,
-        route: req.route,
-        cookies: req.cookies,
-        ip: req.ip,
-        path: req.path,
-        host: req.host,
-        fresh: req.fresh,
-        stale: req.stale,
-        protocol: req.protocol,
-        secure: req.secure,
-        originalUrl: req.originalUrl
-    });
-    console.log("body: " + util.inspect(req.body));
-    console.log("headers: " + req.headers);
-    console.log("trailers: " + req.trailers);
-    console.log("method: " + req.method);
-    console.log("url: " + req.url);
-    console.log("params: " + util.inspect(req.params));
-    console.log("query: " + util.inspect(req.query));
-    console.log("route: " + req.route);
-    console.log("cookies: " + req.cookies);
-    console.log("ip: " + req.ip);
-    console.log("path: " + req.path);
-    console.log("host: " + req.host);
-    console.log("fresh: " + req.fresh);
-    console.log("stale: " + req.stale);
-    console.log("protocol: " + req.protocol);
-    console.log("secure: " + req.secure);
-    console.log("originalUrl: " + req.originalUrl);
+  exports.logExecuteData.push({
+    body: req.body,
+    headers: req.headers,
+    trailers: req.trailers,
+    method: req.method,
+    url: req.url,
+    params: req.params,
+    query: req.query,
+    route: req.route,
+    cookies: req.cookies,
+    ip: req.ip,
+    path: req.path,
+    host: req.host,
+    fresh: req.fresh,
+    stale: req.stale,
+    protocol: req.protocol,
+    secure: req.secure,
+    originalUrl: req.originalUrl,
+  });
+  console.log("body: " + util.inspect(req.body));
+  console.log("headers: " + req.headers);
+  console.log("trailers: " + req.trailers);
+  console.log("method: " + req.method);
+  console.log("url: " + req.url);
+  console.log("params: " + util.inspect(req.params));
+  console.log("query: " + util.inspect(req.query));
+  console.log("route: " + req.route);
+  console.log("cookies: " + req.cookies);
+  console.log("ip: " + req.ip);
+  console.log("path: " + req.path);
+  console.log("host: " + req.host);
+  console.log("fresh: " + req.fresh);
+  console.log("stale: " + req.stale);
+  console.log("protocol: " + req.protocol);
+  console.log("secure: " + req.secure);
+  console.log("originalUrl: " + req.originalUrl);
 }
 
 /*
  * POST Handler for / route of Activity (this is the edit route).
  */
 exports.edit = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-    logData(req);
-    res.send(200, 'Edit');
+  // Data from the req and put it in an array accessible to the main app.
+  //console.log( req.body );
+  connect().then(() =>
+    createSuggestion({ ...suggestion, Reason_vod__c: `edit` })
+  );
+  logData(req);
+  res.send(200, "Edit");
 };
 
 /*
  * POST Handler for /save/ route of Activity.
  */
 exports.save = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-    logData(req);
-    res.send(200, 'Save');
+  // Data from the req and put it in an array accessible to the main app.
+  //console.log( req.body );
+  connect().then(() =>
+    createSuggestion({ ...suggestion, Reason_vod__c: `save` })
+  );
+  logData(req);
+  res.send(200, "Save");
 };
 
 /*
  * POST Handler for /execute/ route of Activity.
  */
 exports.execute = function (req, res) {
+  connect().then(() =>
+    createSuggestion({ ...suggestion, Reason_vod__c: `execute` })
+  );
 
-    // example on how to decode JWT
-    JWT(req.body, process.env.jwtSecret, (err, decoded) => {
+  // example on how to decode JWT
+  JWT(req.body, process.env.jwtSecret, (err, decoded) => {
+    // verification error -> unauthorized request
+    if (err) {
+      console.error(err);
+      return res.status(401).end();
+    }
 
-        // verification error -> unauthorized request
-        if (err) {
-            console.error(err);
-            return res.status(401).end();
-        }
+    if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
+      // decoded in arguments
+      var decodedArgs = decoded.inArguments[0];
 
-        if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-            
-            // decoded in arguments
-            var decodedArgs = decoded.inArguments[0];
-            
-            logData(req);
-            res.send(200, 'Execute');
-        } else {
-            console.error('inArguments invalid.');
-            return res.status(400).end();
-        }
-    });
+      logData(req);
+      res.send(200, "Execute");
+    } else {
+      console.error("inArguments invalid.");
+      return res.status(400).end();
+    }
+  });
 };
-
 
 /*
  * POST Handler for /publish/ route of Activity.
  */
 exports.publish = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-    logData(req);
-    res.send(200, 'Publish');
+  // Data from the req and put it in an array accessible to the main app.
+  //console.log( req.body );
+  connect().then(() =>
+    createSuggestion({ ...suggestion, Reason_vod__c: `Publish` })
+  );
+  logData(req);
+  res.send(200, "Publish");
 };
 
 /*
  * POST Handler for /validate/ route of Activity.
  */
 exports.validate = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-    logData(req);
-    res.send(200, 'Validate');
+  // Data from the req and put it in an array accessible to the main app.
+  //console.log( req.body );
+  connect().then(() =>
+    createSuggestion({ ...suggestion, Reason_vod__c: `validate` })
+  );
+  logData(req);
+  res.send(200, "Validate");
 };
