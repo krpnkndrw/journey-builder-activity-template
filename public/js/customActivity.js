26844945cc0db2f417cb0610be2f21d8bfa4c450
Андрей Карпенко <a.karpenko@onpoint.ru>
2021-10-05 12:19:26 +0300

    Revert "updating custom activity template"
    This reverts commit a10d768cecf5ce7b2ac85ea96ccf55d3ab75a16e.

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,175 +1,106 @@
-define(["postmonger"], function (Postmonger) {
-  "use strict";
+define([
+    'postmonger'
+], function (
+    Postmonger
+) {
+    'use strict';
 
-  var connection = new Postmonger.Session();
-  var authTokens = {};
-  var payload = {};
-  $(window).ready(onRender);
+    var connection = new Postmonger.Session();
+    var authTokens = {};
+    var payload = {};
+    $(window).ready(onRender);
 
-  const getgetget = async (path) => {
-    const response = await fetch(path);
-    const body = await response.json();
-    console.log(body);
-  };
+    connection.on('initActivity', initialize);
+    connection.on('requestedTokens', onGetTokens);
+    connection.on('requestedEndpoints', onGetEndpoints);
+    connection.on('requestedInteraction', onRequestedInteraction);
+    connection.on('requestedTriggerEventDefinition', onRequestedTriggerEventDefinition);
+    connection.on('requestedDataSources', onRequestedDataSources);
 
-  /*const createSuggestion = async (suggestion) => {
-    const response = await fetch("/createSuggestion", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(suggestion),
-    });
-    const parsedResponse = await response.json();
-    console.log(parsedResponse);
-  };*/
+    connection.on('clickedNext', save);
+   
+    function onRender() {
+        // JB will respond the first time 'ready' is called with 'initActivity'
+        connection.trigger('ready');
 
-  /*const suggestion = {
-    Title_vod__c: "Ваше письмо не прочитали",
-    Account_vod__c: "001f000001iIxQ9AAK",
-    Expiration_Date_vod__c: "2021-10-17",
-    Record_Type_Name_vod__c: "Email_vod",
-    Priority_vod__c: "Urgent_vod",
-  };*/
-  //let i = 0;
-  //setTimeout(() => getgetget("/test"), 1000);
-  /*setInterval(() => {
-    createSuggestion({
-      ...suggestion,
-      Reason_vod__c: `a ${i} ${JSON.stringify(payload)}`,
-    });
-    i += 1;
-  }, 10000);*/
+        connection.trigger('requestTokens');
+        connection.trigger('requestEndpoints');
+        connection.trigger('requestInteraction');
+        connection.trigger('requestTriggerEventDefinition');
+        connection.trigger('requestDataSources');  
 
-  connection.on("initActivity", initialize);
-  connection.on("requestedTokens", onGetTokens);
-  connection.on("requestedEndpoints", onGetEndpoints);
-  connection.on("requestedInteraction", onRequestedInteraction);
-  connection.on(
-    "requestedTriggerEventDefinition",
-    onRequestedTriggerEventDefinition
-  );
-  connection.on("requestedDataSources", onRequestedDataSources);
-  connection.on("updateSteps", onUpdateSteps);
-  connection.on("gotoStep", onGotoStep);
-  connection.on("requestedCulture", onRequestedCulture);
-  connection.on("requestedInteractionDefaults", onRequestedInteractionDefaults);
-  connection.on("requestedInteraction", onRequestedInteraction);
-  connection.on(
-    "requestedTriggerEventDefinition",
-    onRequestedTriggerEventDefinition
-  );
-
-  connection.on("clickedNext", save);
-
-  function onRender() {
-    // JB will respond the first time 'ready' is called with 'initActivity'
-    connection.trigger("ready");
-
-    connection.trigger("requestTokens");
-    connection.trigger("requestEndpoints");
-    connection.trigger("requestInteraction");
-    connection.trigger("requestTriggerEventDefinition");
-    connection.trigger("requestDataSources");
-  }
-
-  function onRequestedTriggerEventDefinition(data) {
-    console.log("*** requestedTriggerEventDefinition ***");
-    console.log(data);
-  }
-
-  function onRequestedInteraction(data) {
-    console.log("*** requestedInteraction ***");
-    console.log(data);
-  }
-
-  function onRequestedInteractionDefaults(data) {
-    console.log("*** requestedInteractionDefaults ***");
-    console.log(data);
-  }
-
-  function onRequestedCulture(data) {
-    console.log("*** onRequestedCulture ***");
-    console.log(data);
-  }
-
-  function onGotoStep(data) {
-    console.log("*** gotoStep ***");
-    console.log(data);
-  }
-
-  function onUpdateSteps(data) {
-    console.log("*** updateSteps ***");
-    console.log(data);
-  }
-
-  function onRequestedDataSources(dataSources) {
-    console.log("*** requestedDataSources ***");
-    console.log(dataSources);
-  }
-
-  function onRequestedInteraction(interaction) {
-    console.log("*** requestedInteraction ***");
-    console.log(interaction);
-  }
-
-  function onRequestedTriggerEventDefinition(eventDefinitionModel) {
-    console.log("*** requestedTriggerEventDefinition ***");
-    console.log(eventDefinitionModel);
-  }
-
-  function initialize(data) {
-    console.log(data);
-    if (data) {
-      payload = data;
     }
 
-    var hasInArguments = Boolean(
-      payload["arguments"] &&
-        payload["arguments"].execute &&
-        payload["arguments"].execute.inArguments &&
-        payload["arguments"].execute.inArguments.length > 0
-    );
+    function onRequestedDataSources(dataSources){
+        console.log('*** requestedDataSources ***');
+        console.log(dataSources);
+        //datasources[0].keyPrefix: "Event.CloudPagesSma-27659de6-7790-fe49-28d5-919c43f38135.
+    }
 
-    var inArguments = hasInArguments
-      ? payload["arguments"].execute.inArguments
-      : {};
+    function onRequestedInteraction (interaction) {    
+        console.log('*** requestedInteraction ***');
+        console.log(interaction);
+     }
 
-    console.log(inArguments);
+     function onRequestedTriggerEventDefinition(eventDefinitionModel) {
+        console.log('*** requestedTriggerEventDefinition ***');
+        console.log(eventDefinitionModel);
+    }
 
-    $.each(inArguments, function (index, inArgument) {
-      $.each(inArgument, function (key, val) {});
-    });
+    function initialize(data) {
+        console.log(data);
+        if (data) {
+            payload = data;
+        }
+        
+        var hasInArguments = Boolean(
+            payload['arguments'] &&
+            payload['arguments'].execute &&
+            payload['arguments'].execute.inArguments &&
+            payload['arguments'].execute.inArguments.length > 0
+        );
 
-    connection.trigger("updateButton", {
-      button: "next",
-      text: "done",
-      visible: true,
-    });
-  }
+        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-  function onGetTokens(tokens) {
-    console.log(tokens);
-    authTokens = tokens;
-  }
+        console.log(inArguments);
 
-  function onGetEndpoints(endpoints) {
-    console.log(endpoints);
-  }
+        $.each(inArguments, function (index, inArgument) {
+            $.each(inArgument, function (key, val) {
+                
+              
+            });
+        });
 
-  function save() {
-    var postcardURLValue = $("#postcard-url").val();
-    var postcardTextValue = $("#postcard-text").val();
+        connection.trigger('updateButton', {
+            button: 'next',
+            text: 'done',
+            visible: true
+        });
+    }
 
-    payload["arguments"].execute.inArguments = [
-      {
-        tokens: authTokens,
-      },
-    ];
+    function onGetTokens(tokens) {
+        console.log(tokens);
+        authTokens = tokens;
+    }
 
-    payload["metaData"].isConfigured = true;
+    function onGetEndpoints(endpoints) {
+        console.log(endpoints);
+    }
 
-    console.log(payload);
-    connection.trigger("updateActivity", payload);
-  }
+    function save() {
+        var postcardURLValue = $('#postcard-url').val();
+        var postcardTextValue = $('#postcard-text').val();
+
+        payload['arguments'].execute.inArguments = [{
+            "tokens": authTokens,
+            "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}"
+        }];
+        
+        payload['metaData'].isConfigured = true;
+
+        console.log(payload);
+        connection.trigger('updateActivity', payload);
+    }
+
+
 });
