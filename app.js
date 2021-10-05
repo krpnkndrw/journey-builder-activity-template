6956d96ad3e13ef15b1326c474ed97bdb89245d1
Андрей Карпенко <a.karpenko@onpoint.ru>
2021-10-05 13:15:25 +0300

    reinit

--- app.js
+++ app.js
@@ -1,93 +1,43 @@
-"use strict";
+'use strict';
 // Module Dependencies
 // -------------------
-var express = require("express");
-var bodyParser = require("body-parser");
-var errorhandler = require("errorhandler");
-var http = require("http");
-// var https = require("https");
-var path = require("path");
-var request = require("request");
-var routes = require("./routes");
-var activity = require("./routes/activity");
-//const jsforce = require("jsforce");
-const CONFIGSF = require("./configSF.json");
-// const ViberBot = require("viber-bot").Bot;
-// const BotEvents = require("viber-bot").Events;
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var errorhandler = require('errorhandler');
+var http        = require('http');
+var path        = require('path');
+var request     = require('request');
+var routes      = require('./routes');
+var activity    = require('./routes/activity');
 
 var app = express();
 
 // Configure Express
-app.set("port", process.env.PORT || 3000);
-app.use(bodyParser.raw({ type: "application/jwt" })); //так прилетает токен
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.raw({type: 'application/jwt'}));
 //app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(express.json());//так нет
 
 //app.use(express.methodOverride());
 //app.use(express.favicon());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Express in Development Mode
-if ("development" == app.get("env")) {
+if ('development' == app.get('env')) {
   app.use(errorhandler());
 }
 
 // HubExchange Routes
-app.get("/", routes.index);
-app.post("/login", routes.login);
-app.post("/logout", routes.logout);
+app.get('/', routes.index );
+app.post('/login', routes.login );
+app.post('/logout', routes.logout );
 
 // Custom Hello World Activity Routes
-app.post("/journeybuilder/save/", activity.save);
-app.post("/journeybuilder/validate/", activity.validate);
-app.post("/journeybuilder/publish/", activity.publish);
-app.post("/journeybuilder/execute/", activity.execute);
+app.post('/journeybuilder/save/', activity.save );
+app.post('/journeybuilder/validate/', activity.validate );
+app.post('/journeybuilder/publish/', activity.publish );
+app.post('/journeybuilder/execute/', activity.execute );
 
-http.createServer(app).listen(app.get("port"), function () {
-  console.log("Express server listening on port " + app.get("port"));
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-/************************************************/
-
-// const conn = new jsforce.Connection({
-//   loginUrl: CONFIGSF.salesforceLoginUrl,
-//   instanceUrl: CONFIGSF.salesforceInstanceUrl,
-// });
-
-// function connect() {
-//   return new Promise((resolve, reject) => {
-//     conn.login(
-//       CONFIGSF.salesforceName,
-//       CONFIGSF.salesforcePassword + CONFIGSF.salesforceSecurityToken,
-//       function (err, res) {
-//         if (err) {
-//           reject(err);
-//         }
-//         resolve(res);
-//       }
-//     );
-//   });
-// }
-
-/*const createSuggestion = (suggestion) => {
-  return new Promise((resolve, reject) => {
-    conn.sobject("Suggestion_vod__c").create(suggestion, function (err, res) {
-      if (err || !res.success) {
-        return reject(err, res);
-      }
-      resolve(res);
-    });
-  });
-};
-app.post("/createSuggestion", (req, res) => {
-  const suggestion = req.body;
-  connect()
-    .then(() => createSuggestion(suggestion))
-    .then((result) => {
-      res.send(JSON.stringify(result));
-    });
-});*/
-// app.get("/test", (req, res) => {
-//   res.send(JSON.stringify("♂♂♂♂"));
-// });
--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,106 +1,102 @@
 define([
-    'postmonger'
+  'postmonger'
 ], function (
-    Postmonger
+  Postmonger
 ) {
-    'use strict';
+  'use strict';
 
-    var connection = new Postmonger.Session();
-    var authTokens = {};
-    var payload = {};
-    $(window).ready(onRender);
+  var connection = new Postmonger.Session();
+  var authTokens = {};
+  var payload = {};
+  $(window).ready(onRender);
 
-    connection.on('initActivity', initialize);
-    connection.on('requestedTokens', onGetTokens);
-    connection.on('requestedEndpoints', onGetEndpoints);
-    connection.on('requestedInteraction', onRequestedInteraction);
-    connection.on('requestedTriggerEventDefinition', onRequestedTriggerEventDefinition);
-    connection.on('requestedDataSources', onRequestedDataSources);
+  connection.on('initActivity', initialize);
+  connection.on('requestedTokens', onGetTokens);
+  connection.on('requestedEndpoints', onGetEndpoints);
+  connection.on('requestedInteraction', onRequestedInteraction);
+  connection.on('requestedTriggerEventDefinition', onRequestedTriggerEventDefinition);
+  connection.on('requestedDataSources', onRequestedDataSources);
 
-    connection.on('clickedNext', save);
-   
-    function onRender() {
-        // JB will respond the first time 'ready' is called with 'initActivity'
-        connection.trigger('ready');
+  connection.on('clickedNext', save);
+ 
+  function onRender() {
+      // JB will respond the first time 'ready' is called with 'initActivity'
+      connection.trigger('ready');
 
-        connection.trigger('requestTokens');
-        connection.trigger('requestEndpoints');
-        connection.trigger('requestInteraction');
-        connection.trigger('requestTriggerEventDefinition');
-        connection.trigger('requestDataSources');  
+      connection.trigger('requestTokens');
+      connection.trigger('requestEndpoints');
+      connection.trigger('requestInteraction');
+      connection.trigger('requestTriggerEventDefinition');
+      connection.trigger('requestDataSources');  
 
-    }
+  }
 
-    function onRequestedDataSources(dataSources){
-        console.log('*** requestedDataSources ***');
-        console.log(dataSources);
-        //datasources[0].keyPrefix: "Event.CloudPagesSma-27659de6-7790-fe49-28d5-919c43f38135.
-    }
+  function onRequestedDataSources(dataSources){
+      console.log('*** requestedDataSources ***');
+      console.log(dataSources);
+  }
 
-    function onRequestedInteraction (interaction) {    
-        console.log('*** requestedInteraction ***');
-        console.log(interaction);
-     }
+  function onRequestedInteraction (interaction) {    
+      console.log('*** requestedInteraction ***');
+      console.log(interaction);
+   }
 
-     function onRequestedTriggerEventDefinition(eventDefinitionModel) {
-        console.log('*** requestedTriggerEventDefinition ***');
-        console.log(eventDefinitionModel);
-    }
+   function onRequestedTriggerEventDefinition(eventDefinitionModel) {
+      console.log('*** requestedTriggerEventDefinition ***');
+      console.log(eventDefinitionModel);
+  }
 
-    function initialize(data) {
-        console.log(data);
-        if (data) {
-            payload = data;
-        }
-        
-        var hasInArguments = Boolean(
-            payload['arguments'] &&
-            payload['arguments'].execute &&
-            payload['arguments'].execute.inArguments &&
-            payload['arguments'].execute.inArguments.length > 0
-        );
+  function initialize(data) {
+      console.log(data);
+      if (data) {
+          payload = data;
+      }
+      
+      var hasInArguments = Boolean(
+          payload['arguments'] &&
+          payload['arguments'].execute &&
+          payload['arguments'].execute.inArguments &&
+          payload['arguments'].execute.inArguments.length > 0
+      );
 
-        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
+      var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-        console.log(inArguments);
+      console.log(inArguments);
 
-        $.each(inArguments, function (index, inArgument) {
-            $.each(inArgument, function (key, val) {
-                
+      $.each(inArguments, function (index, inArgument) {
+          $.each(inArgument, function (key, val) {
               
-            });
-        });
+            
+          });
+      });
 
-        connection.trigger('updateButton', {
-            button: 'next',
-            text: 'done',
-            visible: true
-        });
-    }
+      connection.trigger('updateButton', {
+          button: 'next',
+          text: 'done',
+          visible: true
+      });
+  }
 
-    function onGetTokens(tokens) {
-        console.log(tokens);
-        authTokens = tokens;
-    }
+  function onGetTokens(tokens) {
+      console.log(tokens);
+      authTokens = tokens;
+  }
 
-    function onGetEndpoints(endpoints) {
-        console.log(endpoints);
-    }
+  function onGetEndpoints(endpoints) {
+      console.log(endpoints);
+  }
 
-    function save() {
-        var postcardURLValue = $('#postcard-url').val();
-        var postcardTextValue = $('#postcard-text').val();
+  function save() {
+      var postcardURLValue = $('#postcard-url').val();
+      var postcardTextValue = $('#postcard-text').val();
 
-        payload['arguments'].execute.inArguments = [{
-            "tokens": authTokens,
-            "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}"
-        }];
-        
-        payload['metaData'].isConfigured = true;
+      payload['arguments'].execute.inArguments = [{
+          "tokens": authTokens
+      }];
+      
+      payload['metaData'].isConfigured = true;
 
-        console.log(payload);
-        connection.trigger('updateActivity', payload);
-    }
-
-
+      console.log(payload);
+      connection.trigger('updateActivity', payload);
+  }
 });
