83a6f2b0b3038fbe18ccee903d3a0ff662c2a11a
Андрей Карпенко <a.karpenko@onpoint.ru>
2021-10-05 13:29:56 +0300

    sug test

--- routes/activity.js
+++ routes/activity.js
@@ -6,6 +6,7 @@ const Path = require('path');
 const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
 var util = require('util');
 var http = require('https');
+const jsforce = require("jsforce");
 
 exports.logExecuteData = [];
 
@@ -71,6 +72,43 @@ exports.save = function (req, res) {
 /*
  * POST Handler for /execute/ route of Activity.
  */
+
+const conn = new jsforce.Connection({
+  loginUrl: CONFIGSF.salesforceLoginUrl,
+  instanceUrl: CONFIGSF.salesforceInstanceUrl,
+});
+function connect() {
+  return new Promise((resolve, reject) => {
+    conn.login(
+      CONFIGSF.salesforceName,
+      CONFIGSF.salesforcePassword + CONFIGSF.salesforceSecurityToken,
+      function (err, res) {
+        if (err) {
+          reject(err);
+        }
+        resolve(res);
+      }
+    );
+  });
+}
+const createSuggestion = (suggestion) => {
+  return new Promise((resolve, reject) => {
+    conn.sobject("Suggestion_vod__c").create(suggestion, function (err, res) {
+      if (err || !res.success) {
+        return reject(err, res);
+      }
+      resolve(res);
+    });
+  });
+};
+const suggestion = {
+  Title_vod__c: "Ваше письмо не прочитали",
+  Account_vod__c: "001f000001iIxQ9AAK",
+  Expiration_Date_vod__c: "2021-06-17",
+  Record_Type_Name_vod__c: "Email_vod",
+  Priority_vod__c: "Urgent_vod",
+  Reason_vod__c: `test 1`,
+};
 exports.execute = function (req, res) {
 
     // example on how to decode JWT
@@ -84,11 +122,16 @@ exports.execute = function (req, res) {
 
         if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
             
+            
             // decoded in arguments
             var decodedArgs = decoded.inArguments[0];
             
             logData(req);
             logData(decodedArgs)
+            connect()
+              .then(() => createSuggestion(suggestion))
+              .then(() => logData("suggestionCreated"));
+
             res.send(200, 'Execute');
         } else {
             console.error('inArguments invalid.');
