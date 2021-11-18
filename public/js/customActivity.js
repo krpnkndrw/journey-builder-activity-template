f28929b8c04e0a7686b8bda6fe74eeb3b59fd2a2
Андрей Карпенко <a.karpenko@onpoint.ru>
2021-11-18 15:05:01 +0300

    del token

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -90,9 +90,9 @@ define([
       var postcardURLValue = $('#postcard-url').val();
       var postcardTextValue = $('#postcard-text').val();
 
-      payload['arguments'].execute.inArguments = [{
-          "tokens": authTokens
-      }];
+    //   payload['arguments'].execute.inArguments = [{
+    //       "tokens": authTokens
+    //   }];
       
       payload['metaData'].isConfigured = true;
 
