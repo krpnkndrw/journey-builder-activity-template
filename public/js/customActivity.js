94233cdf1532b067d11ccc175fdd0ed10f9f2139
Андрей Карпенко <a.karpenko@onpoint.ru>
2021-11-24 13:32:27 +0300

    fx

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -96,7 +96,7 @@ define([
       
       payload['metaData'].isConfigured = true;
 
-      console.log(JSON.parse(payload));
+      console.log(JSON.stringify(payload));
       connection.trigger('updateActivity', payload);
   }
 
